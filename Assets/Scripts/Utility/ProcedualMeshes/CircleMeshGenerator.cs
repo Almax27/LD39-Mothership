f3a8fa5dfc8ddfff4240d70dfe842801f70c2c1a
Aaron Baumbach <almaxmasuma@gmail.com>
2017-07-31 22:55:02 +0100

    Added line texture to sleection circles

--- Assets/Scripts/Utility/ProcedualMeshes/CircleMeshGenerator.cs
+++ Assets/Scripts/Utility/ProcedualMeshes/CircleMeshGenerator.cs
@@ -3,10 +3,17 @@ using UnityEngine;
 
 public class CircleMeshGenerator : MonoBehaviour
 {
+    public enum UVType
+    {
+        Radial,
+        Centered
+    }
+
     public float innerRadius = 0;
     public float outerRadius = 1;
     [Range(6, 9999)]
     public int segmentCount = 6;
+    public UVType uvType = UVType.Radial;
 
     public MeshRenderer meshRenderer = null;
     public MeshFilter meshFilter = null;
@@ -19,35 +26,62 @@ public class CircleMeshGenerator : MonoBehaviour
         segmentCount = Mathf.Max(6, segmentCount);
 
         List<Vector3> vertices = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
         List<int> triangleList = new List<int>();
 
-        float stepAngle = 360.0f / segmentCount;
-        float angle = 0;
-        vertices.Add(Vector3.up * innerRadius);
-        vertices.Add(Vector3.up * outerRadius);
-        for (int i = 0; i < segmentCount; i++)
+        if (outerRadius > 0)
         {
-            angle += stepAngle;
-            vertices.Add(Quaternion.Euler(0, 0, angle) * Vector3.up * innerRadius);
-            vertices.Add(Quaternion.Euler(0, 0, angle) * Vector3.up * outerRadius);
-        }
+            float stepAngle = 360.0f / segmentCount;
+            float angle = 0;
+            vertices.Add(Vector3.up * innerRadius);
+            vertices.Add(Vector3.up * outerRadius);
+            for (int i = 0; i < segmentCount; i++)
+            {
+                angle += stepAngle;
+                vertices.Add(Quaternion.Euler(0, 0, angle) * Vector3.up * innerRadius);
+                vertices.Add(Quaternion.Euler(0, 0, angle) * Vector3.up * outerRadius);
+            }
 
-        for (int i = 0; i < segmentCount; i++)
-        {
-            int startIndex = i * 2;
-            triangleList.Add(startIndex);
-            triangleList.Add(startIndex + 1);
-            triangleList.Add(startIndex + 2);
+            switch (uvType)
+            {
+            case UVType.Radial:
+                float step = 1.0f / segmentCount;
+                float t = 0;
+                uvs.Add(new Vector2(0, 0));
+                uvs.Add(new Vector2(0, 1));
+                for (int i = 0; i < segmentCount; i++)
+                {
+                    t += step;
+                    uvs.Add(new Vector2(t, 0));
+                    uvs.Add(new Vector2(t, 1));
+                }
+                break;
+            case UVType.Centered:
+                for (int i = 0; i < vertices.Count; i++)
+                {
+                    uvs.Add(new Vector2(vertices[i].x, vertices[i].z) / outerRadius);
+                }
+                break;
+            }
 
-            triangleList.Add(startIndex + 1);
-            triangleList.Add(startIndex + 3);
-            triangleList.Add(startIndex + 2);
+            for (int i = 0; i < segmentCount; i++)
+            {
+                int startIndex = i * 2;
+                triangleList.Add(startIndex);
+                triangleList.Add(startIndex + 1);
+                triangleList.Add(startIndex + 2);
+
+                triangleList.Add(startIndex + 1);
+                triangleList.Add(startIndex + 3);
+                triangleList.Add(startIndex + 2);
+            }
         }
 
         // Create the mesh
         Mesh msh = new Mesh();
         msh.vertices = vertices.ToArray();
         msh.triangles = triangleList.ToArray();
+        msh.uv = uvs.ToArray();
         msh.RecalculateNormals();
         msh.RecalculateBounds();
 
